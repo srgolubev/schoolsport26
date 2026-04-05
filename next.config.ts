cb7b59800bc4164ecdc7789323ff265ee2d56e7b
srgolubev <srgolubev@gmail.com>
2026-04-05 03:23:42 +0300

    fix: ignore media and DB files from webpack watch to prevent recompilation loop

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,21 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Ignore media uploads and DB files from triggering recompilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/public/media/**',
+        '**/media/**',
+        '**/*.db',
+        '**/*.db-shm',
+        '**/*.db-wal',
+        '**/node_modules/**',
+      ],
+    }
+    return config
+  },
 }
 
 export default withPayload(nextConfig)
